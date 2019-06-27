bce3d8f3b7cfa3a853dc46a8c72f6c7d8b8f6ca8
sulongfei001 <30561664+sulongfei001@users.noreply.github.com>
2019-06-27 21:23:32 +0800

    测试jenkins

--- src/main/java/com/sulongfei/jump/config/RestConfig.java
+++ src/main/java/com/sulongfei/jump/config/RestConfig.java
@@ -28,6 +28,7 @@ public class RestConfig {
 
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        System.out.println("===============================================aaaaaa====");
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(30 * 3000);
         factory.setReadTimeout(30 * 3000);
